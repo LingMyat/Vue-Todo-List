5cce8fedd14b79dd6df7476aea445ea387c57307
LingMyat223 <lingmyataung@outlook.com>
2022-12-03 11:54:38 +0630

    login logout feature added and UI Fixed

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var app = new Vue({
     el:'#app',
     data:{
-        Title : 'Things to do',
+        Title : 'My Tasks',
+        // name : localStorage.getItem('name')==null||localStorage.getItem('name')==""?'':localStorage.getItem('name'),
+        name : [null,""].includes(localStorage.getItem('name'))?'':localStorage.getItem('name'),
+        login : [null,""].includes(localStorage.getItem('name'))?false:true,
         tesk : '',
         time : '',
         todos:localStorage.getItem('mytodos')==null?[]:JSON.parse(localStorage.getItem('mytodos')),
@@ -21,14 +24,32 @@ var app = new Vue({
             this.todos.splice(index,1);
             this.storeTodos();
         },
+        addName(){
+            this.storeName();
+            this.login = true;
+        },
+        logout(){
+            this.clearAll();
+            this.name = [];
+            this.storeName();
+            this.login = false;
+        },
+        clearAll(){
+            this.todos = [];
+            this.storeTodos();
+        },
+        storeName(){
+            localStorage.setItem('name',this.name);
+        },
         storeTodos(){
             localStorage.setItem('mytodos',JSON.stringify(this.todos));
-        }
+        },
+        
     },
     computed : {
         text(){
             if (this.todos.length==0) {
-                return "There is no tesk in your todo list ."
+                return "There is no tesk in your list ."
             }
         },
     }
